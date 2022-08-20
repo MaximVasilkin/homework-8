778d72466281909c996ee3d80cc42a8f3d5df713
Maxim Vasilkin <mxvas@mail.ru>
2022-08-20 21:29:53 +0400

    task-2 resolved

--- main.py
+++ main.py
@@ -20,3 +20,38 @@ if __name__ == '__main__':
 
 # Задача-2.
 
+import os
+
+
+class YaDisk():
+    def __init__(self, token):
+        self.token = token
+
+    def uplaod_file(self, path_from, path_to=''):
+        with open(path_from, encoding='utf-8') as file:
+            file_name = os.path.basename(file.name)
+        self.path_from = path_from
+        self.path_to = f'{path_to}/{file_name}'
+        self._url = r'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        self._params = {'path': self.path_to, 'owerwrite': 'true'}
+        self._headers = {'Content-Type': 'application/json', 'Authorization': f'OAuth {self.token}'}
+        self.link = requests.get(self._url, params=self._params, headers=self._headers).json().get('href', None)
+        print(requests.put(self.link, data=open(path_from, 'rb')))
+
+    def delete_file(self, path, permanently='false'):
+        self.path = path
+        self._url = r'https://cloud-api.yandex.net/v1/disk/resources'
+        self._params = {'path': self.path, 'permanently': permanently}
+        self._headers = {'Content-Type': 'application/json', 'Authorization': f'OAuth {self.token}'}
+        print(requests.delete(self._url, params=self._params, headers=self._headers))
+
+
+if __name__ == '__main__':
+    disk = YaDisk('token')
+    disk.uplaod_file('тестовый документ (1).txt')
+    #disk.delete_file('тестовый документ (1).txt')
+
+
+
+
+
